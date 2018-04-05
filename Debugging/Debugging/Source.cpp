2001e2e27722206ed56cdbc97579821cdeb363b8
jthai2 <jimmythai35@gmail.com>
2018-04-05 10:06:05 -0500

    l

---
+++ Debugging/Debugging/Source.cpp
@@ -0,0 +1,22 @@
+//SwatTheBugs
+//04/5/2018
+//Jimmy Thai
+
+#include <iostream>
+#include <string>>
+using namespace std;
+
+int main()
+{
+	//declare variable
+	double temp = 35.3;
+
+	//increase variable's value by 1.5
+	temp = temp + 1.5;
+
+		//display variable's value
+		cout << "After adding 1.5, the temp variable now contains " << temp << "." << std::endl;
+
+	system("pause");
+	return 0;
+}	//end of main function
