ae3aa51f991924b81b4f21ebd2aa7b2f923efabf
jthai2 <jimmythai35@gmail.com>
2018-04-05 13:28:56 -0500

    l

---
+++ Eggs/Eggs/Source.cpp
@@ -0,0 +1,16 @@
+//Eggs
+//04/5/2018
+//Jimmy Thai
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	double dozenEggs = 2;
+
+	std::cout << "How many eggs would you like?" << std::endl;
+
+
+
+}
