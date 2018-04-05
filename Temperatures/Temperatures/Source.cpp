3ae568e8b26e06254b2b42d8826de3ea4cb61a1c
jthai2 <jimmythai35@gmail.com>
2018-04-05 13:15:14 -0500

    l

--- Temperatures/Temperatures/Source.cpp
+++ Temperatures/Temperatures/Source.cpp
@@ -1,3 +1,31 @@
 //Temperatures
 //04/4/18
 //Jimmy Thai
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	double temperatureMorning;
+	double temperatureNoon;
+	double temperatureNight;
+
+	double celsiusMorning = 10;
+	double celsiusNoon = 20;
+	double celsiusNight = 30;
+
+	temperatureMorning = celsiusMorning * (9 / 5) + 32;
+	temperatureNoon = celsiusNoon * (9 / 5) + 32;
+	temperatureNight = celsiusNight * (9 / 5) + 32;
+
+	std::cout << "The temperature at 8:00 a.m. is " << celsiusMorning << " celsius and " << temperatureMorning << " fahrenheit." << std::endl;
+
+	std::cout << "The temperature at 5:00 p.m. is " << celsiusNoon << " celsius and " << temperatureNoon << " fahrenheit." << std::endl;
+
+	std::cout << "The temperature at 12:00 p.m.is " << celsiusNight << " celsius and " << temperatureNight << " fahrenheit." << std::endl;
+
+	system("pause");
+	return 0;
+
+}
