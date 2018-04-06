9c2e310d087d4f01c29a4df06732ffd03b51d998
jthai2 <jimmythai35@gmail.com>
2018-04-06 13:00:59 -0500

    l

--- Eggs/Eggs/Source.cpp
+++ Eggs/Eggs/Source.cpp
@@ -7,10 +7,28 @@
 
 int main()
 {
-	double dozenEggs = 2;
+	int eggsWanted;
+	int dozenEggs = 12;
 
 	std::cout << "How many eggs would you like?" << std::endl;
+	std::cin >> eggsWanted;
 
+	int dozenEggsTotal = eggsWanted / dozenEggs;
+	int singleEggTotal = eggsWanted % dozenEggs;
 
+	int dozenEggsTotalPrice = dozenEggsTotal + dozenEggsTotal;
+	double singleEggTotalPrice = singleEggTotal * .25;
 
+	int discountedEgg = dozenEggsTotal * 1.50;
+
+	std::cout << "You have " << dozenEggsTotal << " dozen eggs and " << singleEggTotal << " single eggs." << std::endl;
+	std::cout << "Your total price per dozen is $" << dozenEggsTotalPrice << ", your total price per remaining egg is $" << singleEggTotalPrice << std::endl;
+
+	if (dozenEggsTotal > 10)
+	{
+		std::cout << "Your discounted total is now $" << discountedEgg << " per dozen egg." << std::endl;
+	}
+
+	system("pause");
+	return 0;
 }
