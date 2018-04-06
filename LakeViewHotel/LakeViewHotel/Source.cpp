a139f06081d2bb4deaa06fc05dd0314d4b7fb968
jthai2 <jimmythai35@gmail.com>
2018-04-06 13:12:17 -0500

    l

---
+++ LakeViewHotel/LakeViewHotel/Source.cpp
@@ -0,0 +1,27 @@
+//LakeViewHotel
+//04/6/2018
+//Jimmy Thai 
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	int nightsPerStay;
+	int perNightRate = 100;
+	int roomServiceCharge = 35;
+	int telephoneCharge = .25;
+
+	std::cout << "How many nights would you like to stay?" << std::endl;
+	std::cin >> nightsPerStay;
+
+	std::cout << "Press 1 if you want room service, press 2 if NO." << std::endl;
+
+	std::cout << "Press 1 if you want " << std::endl;
+
+	std::cout << "Your total is " << std::endl;
+
+	system("pause");
+	return 0;
+
+}
