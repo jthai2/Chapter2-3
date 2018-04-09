4a2b104368e29836ab5015948564c3f7e5b43d55
jthai2 <jimmythai35@gmail.com>
2018-04-09 12:25:21 -0500

    l

--- LakeViewHotel/LakeViewHotel/Source.cpp
+++ LakeViewHotel/LakeViewHotel/Source.cpp
@@ -8,6 +8,9 @@
 int main()
 {
 	int nightsPerStay;
+	int nightsCost;
+	int roomService;
+	int useTelephone;
 	int perNightRate = 100;
 	int roomServiceCharge = 35;
 	int telephoneCharge = .25;
@@ -15,11 +18,15 @@ int main()
 	std::cout << "How many nights would you like to stay?" << std::endl;
 	std::cin >> nightsPerStay;
 
-	std::cout << "Press 1 if you want room service, press 2 if NO." << std::endl;
+	std::cout << "You want room service?" << std::endl;
+	std::cin >> roomService;
 
-	std::cout << "Press 1 if you want " << std::endl;
+	std::cout << "You want to use a telephone?" << std::endl;
+	std::cin >> useTelephone;
 
-	std::cout << "Your total is " << std::endl;
+	nightsCost = nightsPerStay * perNightRate;
+
+	std::cout << "Your total is $" << nightsCost << std::endl;
 
 	system("pause");
 	return 0;
