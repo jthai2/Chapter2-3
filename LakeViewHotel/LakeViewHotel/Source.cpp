4bc7baef746a054d9f636aba9f93970f63d77ef9
jthai2 <jimmythai35@gmail.com>
2018-04-10 22:30:33 -0500

    l

--- LakeViewHotel/LakeViewHotel/Source.cpp
+++ LakeViewHotel/LakeViewHotel/Source.cpp
@@ -3,32 +3,42 @@
 //Jimmy Thai 
 
 #include <iostream>
-#include <string>
 
 int main()
 {
-	int nightsPerStay;
-	int nightsCost;
+	double nightsPerStay;
+	int roomServiceChoice;
 	int roomService;
-	int useTelephone;
-	int perNightRate = 100;
-	int roomServiceCharge = 35;
-	int telephoneCharge = .25;
+	double phoneMinutes;
+	double bill;
 
-	std::cout << "How many nights would you like to stay?" << std::endl;
+	std::cout << "How many nights have you stayed?" << std::endl;
 	std::cin >> nightsPerStay;
+	std::cout << "Do you want room service? Press 1 for Yes and 2 for No>>>" << std::endl;
+	std::cin >> roomServiceChoice;
 
-	std::cout << "You want room service?" << std::endl;
-	std::cin >> roomService;
+	if (roomServiceChoice == 1)
+	{
+		roomService = 1;
+	}
+	else if (roomServiceChoice == 2)
+	{
+		roomService = 2;
+	}
+	else
+	{
+		std::cout << "What you entered was invalid, automatically setting room service in." << std::endl;
+		roomService = 1;
+	}
 
-	std::cout << "You want to use a telephone?" << std::endl;
-	std::cin >> useTelephone;
+	std::cout << "How much minutes have you spent on the phone?" << std::endl;
+	std::cin >> phoneMinutes;
 
-	nightsCost = nightsPerStay * perNightRate;
+	bill = (nightsPerStay * 100) + (roomService * 35) + (phoneMinutes * 0.25);
 
-	std::cout << "Your total is $" << nightsCost << std::endl;
-
-	system("pause");
-	return 0;
-
+	std::cout << "Your bill is $" << bill << std::endl;
+	std::cout << endl;
+	
+	system("Pause");
+	return(0);
 }
